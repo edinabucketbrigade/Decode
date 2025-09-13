ed49333b952902fd1ad1aa4efbaeebdd20d23fed
mwollnik99 <74638220+mwollnik99@users.noreply.github.com>
2025-09-12 20:10:58 -0500

    Initial opcode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opcodes/FirstOpcode.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opcodes;
+
+import dev.nextftc.ftc.NextFTCOpMode;
+
+
+public class FirstOpcode extends NextFTCOpMode {
+    {
+        addComponents(/* vararg components */);
+    }
+
+    @Override public void onInit() { }
+    @Override public void onWaitForStart() { }
+    @Override public void onStartButtonPressed() { }
+    @Override public void onUpdate() { }
+    @Override public void onStop() { }
+}
