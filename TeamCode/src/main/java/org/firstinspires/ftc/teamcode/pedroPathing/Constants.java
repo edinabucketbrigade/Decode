52aa0fc1e1d76661c70ccb66975dfc0b2ed55f92
mwollnik99 <74638220+mwollnik99@users.noreply.github.com>
2025-09-16 19:52:29 -0500

    Pedro Constants set

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -11,13 +11,19 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
+    public static double ForwardZeroPowerAcceleration = 1.0;
+    public static double LateralZeroPowerAcceleration = 1.0;
+
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(5)
-            //.forwardZeroPowerAcceleration(1.0)
-            //.lateralZeroPowerAcceleration(1.0)
+            //.forwardZeroPowerAcceleration(ForwardZeroPowerAcceleration)
+            //.lateralZeroPowerAcceleration(LateralZeroPowerAcceleration)
             ;
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static double XVelocity = 1.0;
+    public static double YVelocity = 1.0;
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -29,24 +35,29 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            //.xVelocity(1)
-            //.yVelocity(1)
+            //.xVelocity(XVelocity)
+            //.yVelocity(YVelocity)
             ;
+    public static double RobotWidth =15.5;
+    public static double RobotLength =17.0;
+    public static double ForwardTicksToInches = 0.005806641378857757;
+    public static double StrafeTicksToInches = 0.006122924855949316;
+    public static double TurnTicksToInches = 0.014282963033037463;
 
     public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
             .rightFrontMotorName("rightFrontDrive")
             .rightRearMotorName("rightBackDrive")
             .leftRearMotorName("leftBackDrive")
             .leftFrontMotorName("leftFrontDrive")
-            .leftFrontEncoderDirection(Encoder.FORWARD)
-            .leftRearEncoderDirection(Encoder.FORWARD)
+            .leftFrontEncoderDirection(Encoder.REVERSE)
+            .leftRearEncoderDirection(Encoder.REVERSE)
             .rightFrontEncoderDirection(Encoder.FORWARD)
             .rightRearEncoderDirection(Encoder.FORWARD)
-            .robotWidth(15.5)
-            .robotLength(17.0)
-            .forwardTicksToInches(1.565518616454974)
-            .strafeTicksToInches(.697478)
-            .turnTicksToInches(.61879856)
+            .robotWidth(RobotWidth)
+            .robotLength(RobotLength)
+            .forwardTicksToInches(ForwardTicksToInches)
+            .strafeTicksToInches(StrafeTicksToInches)
+            .turnTicksToInches(TurnTicksToInches)
             ;
 
     public static Follower createFollower(HardwareMap hardwareMap) {
