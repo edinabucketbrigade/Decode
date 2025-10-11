66fa3f7bf6f9d69169617e0313ea5e03460308c4
Keoni262 <keoniquintana@icloud.com>
2025-10-11 11:45:34 -0500

    Intake.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opcodes/Intake.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.opcodes;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.hardware.motors.Motor;
+
+
+
+@Configurable
+public class Intake extends SubsystemBase {
+    private Motor flywheel;
+    public static double kP = 20;
+    public static double kV = 0.7;
+    public static double speed = 6000.0;
+    public Intake(HardwareMap hardwareMap) {
+
+        flywheel = new Motor(hardwareMap, "flywheel_Main", Motor.GoBILDA.BARE);
+        flywheel.setRunMode(Motor.RunMode.VelocityControl);
+        flywheel.setVeloCoefficients(kP, 0, 0);
+        flywheel.setFeedforwardCoefficients(0, kV);
+    }
+
+    public void StartIntake() {
+
+        flywheel.set(speed);
+
+
+    }
+
+    public void StopIntake() {
+
+        flywheel.set(0);
+    }
+}
